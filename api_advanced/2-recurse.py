efbe941a311511e365f77004bb03fecb462e2fd6
vumugwaneza <116736307+vanessaU4@users.noreply.github.com>
2023-07-14 14:18:49 +0000

    again

--- api_advanced/2-recurse.py
+++ api_advanced/2-recurse.py
@@ -1,33 +1,27 @@
 #!/usr/bin/python3
-""" a recursive function that queries the Reddit API """
+"""docs"""
 import requests
 
 
 def recurse(subreddit, hot_list=[], after=None):
-    """ returns list with titles """
-    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    params = {'after': after}
-    response = requests.get(
-                                url,
-                                headers=headers,
-                                params=params,
-                                allow_redirects=False
-                            )
-    if response.status_code == 200:
-        data = response.json().get('data')
-        if data is not None:
-            children = data.get('children')
-            if children is not None:
-                for child in children:
-                    hot_list.append(child.get('data').get('title'))
-                after = data.get('after')
-                if after is not None:
-                    return recurse(subreddit, hot_list, after)
-                else:
-                    return hot_list
-        else:
-            return hot_list
+    """"Doc"""
+    url = "https://www.reddit.com/r/{}/hot.json" \
+        .format(subreddit)
+    header = {'User-Agent': 'Mozilla/5.0'}
+    param = {'after': after}
+    resopnse = requests.get(url, headers=header, params=param)
+
+    if resopnse.status_code != 200:
+        return None
     else:
-        return ok
-        
+        json_res = resopnse.json()
+        after = json_res.get('data').get('after')
+        has_next = \
+            json_res.get('data').get('after') is not None
+        hot_articles = json_res.get('data').get('children')
+        [hot_list.append(article.get('data').get('title'))
+         for article in hot_articles]
+
+        return recurse(subreddit, hot_list, after=after) \
+            if has_next else hot_list
+            
